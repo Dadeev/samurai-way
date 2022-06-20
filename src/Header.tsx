d33fce93c9b5f4f22f775e819ba3c7185e5e0939
Tima <dadeev.tim@gmail.com>
2022-06-20 11:47:19 +0700

    feat: implement import and export for new files (lesson 09)

---
+++ src/Header.tsx
@@ -0,0 +1,12 @@
+
+const Technologies = () => {
+    return (
+        <div>
+            <a href='#'>Home</a>
+            <a href='#'>News Feed</a>
+    <a href='#'>Messages</a>
+        </div>
+)
+}
+
+export default Technologies;
---
+++ src/Technologies.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Technologies = () => {
+    return (
+        <div>
+            <a href='#'>Home</a>
+            <a href='#'>News Feed</a>
+    <a href='#'>Messages</a>
+        </div>
+)
+}
