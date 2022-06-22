4de62174ace74b3fc6b95c68208e0241a51e8c0c
Tima <dadeev.tim@gmail.com>
2022-06-22 20:24:45 +0700

    feat:add modules for css components

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Header.module.css';
 
 export const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img
                 src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/business-logo-design-template-78655edda18bc1196ab28760f1535baa_screen.jpg?ts=1617645324"
                 alt=""/>
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from'./Profile.module.css'
 
 export const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" alt=""/>
             </div>
