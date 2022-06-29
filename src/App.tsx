bf68e3aa654a4ad6576238423d99995dc785a8da
Tima <dadeev.tim@gmail.com>
2022-06-29 15:38:02 +0700

    feat: add some components

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import {Header} from "./Components/Header/Header";
 import {Navbar} from "./Components/Navbar/Navbar";
 import {Profile} from "./Components/Profile/Profile";
 import {Dialogs} from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Profile/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/dialogs' element={<Dialogs/>}/>
+                    </Routes>
+                </div>
             </div>
-            {/*<Profile/>*/}
-        </div>
+        </BrowserRouter>
     );
 }
 
