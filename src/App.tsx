acccc0114518c4fb40024e031d2ee6715623a541
Tima <dadeev.tim@gmail.com>
2022-06-29 16:42:44 +0700

    feat: add some route components

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import {Navbar} from "./Components/Navbar/Navbar";
 import {Profile} from "./Components/Profile/Profile";
 import {Dialogs} from "./Components/Dialogs/Dialogs";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {News} from "./Components/News/News";
+import {Music} from "./Components/Music/Music";
+import {Settings} from "./Components/Settings/Settings";
 
 function App() {
     return (
@@ -16,6 +19,9 @@ function App() {
                     <Routes>
                         <Route path='/profile' element={<Profile/>}/>
                         <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
                     </Routes>
                 </div>
             </div>
