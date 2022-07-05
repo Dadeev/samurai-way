08fff9e517e9473f44b7ee0ddd0182d186be8089
Tima <dadeev.tim@gmail.com>
2022-07-05 12:13:21 +0700

    refactor: implement method map for some data

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -26,22 +26,50 @@ const Message: React.FC<MessageType> = (props) => {
     )
 }
 
+type dialogsDataType = {
+    id: number
+    name: string
+}
 
+type messagesDataType = {
+    id: number
+    message: string
+}
 export const Dialogs = () => {
+
+    let dialogsData: dialogsDataType[] = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Victor'},
+        {id: 6, name: 'Valera'},
+    ]
+
+    let messagesData: messagesDataType[] = [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How is your It-camasutra?'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Have a nice day'},
+        {id: 5, message: 'It is a good month'},
+        {id: 6, message: 'it incubator!'},
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={'Dimych'} id={1}/>
-                <DialogItem name={'Andrey'} id={2}/>
-                <DialogItem name={'Sveta'} id={3}/>
-                <DialogItem name={'Sasha'} id={4}/>
-                <DialogItem name={'Victor'} id={5}/>
-                <DialogItem name={'Valera'} id={6}/>
+                {dialogsData.map((el) => {
+                    return (
+                        <DialogItem key={el.id} name={el.name} id={el.id}/>
+                    )
+                })}
             </div>
             <div className={s.messages}>
-                <Message message={'Hello'}/>
-                <Message message={'How is your It-camasutra?'}/>
-                <Message message={'Yo'}/>
+                {messagesData.map((el) => {
+                    return (
+                        <Message key={el.id} message={el.message}/>
+                    )
+                })}
             </div>
         </div>
     )
