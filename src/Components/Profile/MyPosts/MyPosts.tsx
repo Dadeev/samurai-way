18386f65e552056f3b652ed364eab2c3a3ea94fd
Tima <dadeev.tim@gmail.com>
2022-07-05 12:20:27 +0700

    refactor: implement method map

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import s from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 
+type postDataType = {
+    id: number
+    message: string
+    likes: number
+}
+
 export const MyPosts = () => {
+    let postData: postDataType[] = [
+        {id: 1, message: 'Hi, how are you?', likes: 15},
+        {id: 1, message: 'It\'s my first post', likes: 20}
+    ]
+
     return (
         <div className={s.postsBlock}>
             <h3>My post</h3>
@@ -15,8 +26,11 @@ export const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={'Hi, how are you?'} likes={15}/>
-                <Post message={"It's my first post"} likes={20}/>
+                {postData.map((el) => {
+                    return (
+                        <Post key={el.id} message={el.message} likes={el.likes}/>
+                    )
+                })}
             </div>
         </div>
     )
