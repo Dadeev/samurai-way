967f3a891563ff287589acb6ab22cd86f875872d
Tima <dadeev.tim@gmail.com>
2022-08-17 17:32:09 +0700

    refactor: implent Side effect into componentDidMount method

--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -35,16 +35,14 @@ export class Users extends React.Component<UserPropsType> {
     //         fullName: 'Sasha',
     //         status: 'Im a boss too',
     //         location: {city: 'Kiev', country: 'Ukraine'}
-    //     },]
-    constructor(props:UserPropsType) {
-        super(props);
-        if (this.props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    this.props.setUsers(response.data.items)
-                })
-        }
+    //     }
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
     }
+
     render() {
         return (
             <>
