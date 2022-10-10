05b3724ce7ef619c990e647df401ee1d79e9d0c6
Tima <dadeev.tim@gmail.com>
2022-10-10 17:57:48 +0700

    feat: imolement 85 lesson

--- src/Components/Profile/ProfileStatusWithHooks.tsx
+++ src/Components/Profile/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 
 type ProfileStatusType = {
     status: string
@@ -8,6 +8,10 @@ type ProfileStatusType = {
 export const ProfileStatusWithHooks = (props: ProfileStatusType) => {
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+
+    useEffect(()=>{
+        setStatus(props.status)
+    }, [props.status])
     const activateEditMode = () => {
         setEditMode(true)
     }
